e7720a3704418b9f7b42fdf0bca6eba1d0a64498
ZaneHannanAU <ZaneHannanAU@users.noreply.github.com>
2017-03-28 19:53:41 +1100

    Splits into shorter pieces. ~400 per each.

--- translate.js
+++ translate.js
@@ -6,28 +6,40 @@ const unsafe = /^(\d+(?::\d+:\d+[\.,]\d+ --> \d+:\d+:\d+[\.,]\d+)?)$/gm
 // In this case, if it only has numbers or a time stamp it's unsafe, so remove and add back later.
 
 
-const translate = module.exports = (
+const translator = module.exports = (
 	o = {from: 'ja', to: 'en', text: 'ばか！'.repeat(3), }
 ) => {
-	o.unsafeTranslate = []
-	o.text.replace(unsafe, (match, line) => {
-		o.unsafeTranslate.push(line)
+	console.warn(`Translating ${o.filename}.${o.from}_${o.to}...`);
+	var unsafeTranslate = []
+	var translate = o.text.replace(unsafe, (match, line) => {
+		unsafeTranslate.push(line)
 		return '\uFFFC'
+	}).split(/(?:\r?\n){2,}/g)
+	var translated = []
+	function writeFile() {
+		return fs.writeFile(
+			`${o.filename}.${o.from}_${o.to}`,
+			translated
+				.join('\r\n\r\n')
+				.replace(/\uFFFC/g, () => unsafeTranslate.shift()),
+			'utf8',
+			err => {
+				if (err) throw err
+				else console.log('Saved %s', `${o.filename}.${o.from}_${o.to}`);
+			})
+	}
+	console.warn(`${JSON.stringify(unsafeTranslate.concat(translate), null, ' ')}\r\nMaking ${translate.length} requests${
+		translate.length > 10 ? ', hold TIGHT!' : '.'
+	}\r\n`);
+	var ntranslated = 0
+	translate.forEach((phrase, i) => {
+		gta(phrase, o)
+			.then(res => res.text)
+			.then(text => {
+				translated[i] = text
+				console.log('%d ::::::::\r\n%s\r\n------\r\n', i, text)
+			})
+			.then(() => translated.length === ++ntranslated ? writeFile() : null)
+			.catch(console.error)
 	})
-
-	gta(o.text, o)
-		.then(res => res.text
-			.replace(/\uFFFC/g, () => o.unsafeTranslate.shift())
-			// adds back the text we just removed lol
-		)
-		// .then(console.log)
-		.then(e => {
-			console.log(e); return e
-		})
-		.then(text => fs.writeFile(
-			`${o.path}.${o.from}_${o.to}`,
-			text,
-			o.filename
-		))
-		.catch(console.error)
 }
