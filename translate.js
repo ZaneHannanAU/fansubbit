e8c41ce8e777a36ca8874b91cc27efca82d74ee9
ZaneHannanAU <ZaneHannanAU@users.noreply.github.com>
2017-03-28 20:27:50 +1100

    Consistent breaks.

--- translate.js
+++ translate.js
@@ -13,22 +13,28 @@ const translator = module.exports = (
 	var unsafeTranslate = []
 	var translate = o.text.replace(unsafe, (match, line) => {
 		unsafeTranslate.push(line)
+		console.log(
+			"Unsafe to translate:", unsafeTranslate[ unsafeTranslate.length-1 ]
+		);
 		return '\uFFFC'
 	}).split(/(?:\r?\n){2,}/g)
 	var translated = []
-	function writeFile() {
+	function writeFile(done) {
+		const fullyTranslated = done
+			.join('\r\n\r\n')
+			.replace(/\uFFFC/g, () => unsafeTranslate.shift())
 		return fs.writeFile(
 			`${o.filename}.${o.from}_${o.to}`,
-			translated
-				.join('\r\n\r\n')
-				.replace(/\uFFFC/g, () => unsafeTranslate.shift()),
+			fullyTranslated,
 			'utf8',
 			err => {
 				if (err) throw err
-				else console.log('Saved %s', `${o.filename}.${o.from}_${o.to}`);
+				else console.log('Saved %s;\r\n\r\n', `${o.filename}.${o.from}_${o.to}`, fullyTranslated);
 			})
 	}
-	console.warn(`${JSON.stringify(unsafeTranslate.concat(translate), null, ' ')}\r\nMaking ${translate.length} requests${
+	console.warn(`${ " "
+		// + JSON.stringify(unsafeTranslate.concat(translate), null, ' ')
+	}\r\nMaking ${translate.length} requests${
 		translate.length > 10 ? ', hold TIGHT!' : '.'
 	}\r\n`);
 	var ntranslated = 0
@@ -37,9 +43,9 @@ const translator = module.exports = (
 			.then(res => res.text)
 			.then(text => {
 				translated[i] = text
-				console.log('%d ::::::::\r\n%s\r\n------\r\n', i, text)
+				console.log('  %d complete:\r\n%s\r\n------\r\n', i, text)
 			})
-			.then(() => translated.length === ++ntranslated ? writeFile() : null)
+			.then(() => translated.length === ++ntranslated ? writeFile( translated ) : null)
 			.catch(console.error)
 	})
 }
